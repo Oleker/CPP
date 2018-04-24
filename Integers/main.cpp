845655bcdf068c8217355d267d4508688fb446c2
Henrique Przybylski Coelho <36766967+Oleker@users.noreply.github.com>
2018-04-24 17:36:32 -0300

    Add files via upload

---
+++ Integers/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int bola = 10;
+    cout <<"A variável bola é: " << bola;
+    
+    int a = 5;
+    int b = 7;
+    int soma = a+b;
+    cout << "\nA soma de " << a  << " e " << b << " é " << soma;
+    
+    return 0;
+}
+
